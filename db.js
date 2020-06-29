00b9ba226b748a10fdd62216ffdd8816a76dfa01
iamcho2 <thatgrass@naver.com>
2020-06-29 19:59:46 +0900

    install mongoDB and mongoose

--- db.js
+++ db.js
@@ -1,36 +1,18 @@
-export const videos = [
-        {
-        id:32435,
-        title: 'Video awesome',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "Nicolas",
-            email: "thecho2cho2@gmail.com"
-        }
-    },{
-        id:235325235,
-        title: 'Video awesome222',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 12111212,
-            name: "Nicolas",
-            email: "thecho2cho2@gmail.com"
-        }
-    },{
-        id:11111,
-        title: 'Video awesome333',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 12123412,
-            name: "Nicolas",
-            email: "thecho2cho2@gmail.com"
-        }
-    }
-]
+import mongoose from 'mongoose'
+import dotenv from "dotenv"
+
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL,{
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ connected to DB");
+const handleError = () => console.log("❌ERROR on DB connection ${error}")
+
+db.once("open", handleOpen);
+db.on("error", handleError);
